483dd168fea4dc1ca674ceaadd0df38301525579
Henrique Claranhan <henrique.claranhan@ufms.br>
2026-07-19 03:23:18 -0400

    feat(pwa): add maskable svg icon with proper padding and adaptive background

--- vite.config.ts
+++ vite.config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'maskable-icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
